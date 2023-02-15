c660718df1256d33853b84a63e71b598d395e43f
kevin <pinerokevin@icloud.com>
2023-02-15 15:20:04 +0100

    Change branch project

---
+++ front-end/src/layout/home/HomeLayout.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="view">
+		<HeaderLayout></HeaderLayout>
+
+		<div class="content pt-12 pb-12">
+			<router-view></router-view>
+		</div>
+
+		<FooterLayout></FooterLayout>
+	</div>
+</template>
+
+<script setup lang="ts">
+import HeaderLayout from './HeaderLayout.vue';
+import FooterLayout from './FooterLayout.vue';
+</script>
